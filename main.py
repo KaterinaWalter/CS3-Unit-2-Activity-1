0696466fd84a60b5f184630c0c283518452cd833
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-10-28 19:09:29 +0000

    finished part B

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ print(df.info()) # summary of columns
 budget_df = df[ df['budget'] > 1000000 ]
 print(budget_df.shape) # 7208 movies have a budget above 1mil
 
+# Create a series from the budget column
+# that uses the title column for the indices
+budget_lookup = pd.Series(budget_df['budget'].values, index=budget_df['title'])
+print(budget_lookup) # now we can look up budgets by movie title!
+print(budget_lookup['Jumanji']) # 65 million
+
+# First define the condition to be checked
+# budget_lookup.index is the title of the movie
+condition = (budget_lookup.index >= 'A Bag of Hammers') & (budget_lookup.index <= 'Byzantium')
+# condition is satisfied when the movie starts with A or B
+budget_lookup_AB = budget_lookup[condition]
+
